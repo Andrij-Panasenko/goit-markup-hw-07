c218e8ad2d1f857e9b02ace6c1c8fe4ad50f91c2
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-08-21 23:23:47 +0300

    added close modal by backdrop click

--- js/modal.js
+++ js/modal.js
@@ -3,13 +3,21 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    backdrop: document.querySelector(".backdrop")
   };
 
+  refs.backdrop.addEventListener("click", onBackdropClick)
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
     document.body.classList.toggle("no-scroll");
+  }
+
+  function onBackdropClick(event) {
+    if (event.target === event.currentTarget) {
+      toggleModal();
+    }
   }
 })();
