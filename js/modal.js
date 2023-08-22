9c1efe103032dd5009f9832a6bd0a9310c3031a2
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-08-22 23:12:03 +0300

    close modal on Esc key

--- js/modal.js
+++ js/modal.js
@@ -3,20 +3,36 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    backdrop: document.querySelector(".backdrop")
+    backdrop: document.querySelector(".backdrop"),
   };
 
-  refs.backdrop.addEventListener("click", onBackdropClick)
+  refs.backdrop.addEventListener("click", modalCloseHandler);
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
     document.body.classList.toggle("no-scroll");
+    window.addEventListener("keydown", onEscPress);
+    
+    if (!refs.modal.classList.contains("is-hidden")) {
+      window.addEventListener("keydown", onEscPress);
+    } else {
+      window.removeEventListener("keydown", onEscPress);
+    }
   }
 
-  function onBackdropClick(event) {
+  function modalCloseHandler(event) {
     if (event.target === event.currentTarget) {
+      toggleModal();
+    }
+  }
+
+  function onEscPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const IsEscKey = event.code === ESC_KEY_CODE;
+
+    if (IsEscKey) {
       toggleModal();
     }
   }
